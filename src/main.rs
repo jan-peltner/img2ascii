24a1afbf2205efc8d6b6296696b4b56ff3120cf0
jan <defPelly@proton.me>
2024-12-18 11:10:15 +0100

    feat: add clap cli arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
+use clap::Parser;
 use image::{imageops, ImageReader};
 use img2ascii::ascii::AsciiArt;
 
 const MAX_WIDTH: u32 = 200;
 const MAX_HEIGHT: u32 = 50;
 
+#[derive(Parser)]
+struct Cli {
+    /// path to image
+    file_path: String,
+}
+
 fn main() {
-    let img = ImageReader::open("assets/bobo.jpg").expect("Could not read image");
+    let cli = Cli::parse();
+    let img = ImageReader::open(&cli.file_path).expect("Could not read image");
     let mut img = img.decode().expect("Could not decode image");
     if img.width() > MAX_WIDTH || img.height() > MAX_HEIGHT {
         img = img.resize(MAX_WIDTH, MAX_HEIGHT, imageops::FilterType::Nearest);
